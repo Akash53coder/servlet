1219c470dfbeea04b08c628d9243e1be7fb991ad
akash m <akash.mabali7@gmail.com>
2021-11-14 11:06:24 +0530

    Servlet Eample on JDBC

--- servletjdbc/src/com/xworkz/dao/XworkzDAO.java
+++ servletjdbc/src/com/xworkz/dao/XworkzDAO.java
@@ -1,5 +1,35 @@
 package com.xworkz.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+  
 public class XworkzDAO {
 
+	public static Connection getConnection() {
+
+		String url="jdbc:mysql://localhost:3306/students";
+		String username = "root";
+		String password = "Akash@006";
+		Connection connection=null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			//sending sql query from java program to db engine we need statement object
+			connection = DriverManager.getConnection(url,username,password);
+
+		}catch(ClassNotFoundException | SQLException e) {
+
+			System.out.println(e.getMessage());
+			
+		}
+		finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return connection;
+
+	}
 }
--- servletjdbc/src/com/xworkz/servletjdbc/ServletJdbc.java
+++ servletjdbc/src/com/xworkz/servletjdbc/ServletJdbc.java
@@ -1,11 +1,17 @@
 package com.xworkz.servletjdbc;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.xworkz.dao.XworkzDAO;
 
 public class ServletJdbc extends HttpServlet {
        
@@ -18,10 +24,29 @@ public class ServletJdbc extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		String name = request.getParameter("name");
-		String yop = request.getParameter("yop");
+//		String yop = request.getParameter("yop");
 		String education = request.getParameter("education");
 		String type = request.getParameter("type");
-		System.out.println(name+" "+yop+" "+education+" "+type);
+		int yopi = Integer.valueOf(request.getParameter("yop"));
+		System.out.println(name+" "+yopi+" "+education+" "+type);
+		
+		
+		try {
+            Connection con = XworkzDAO.getConnection();
+            PreparedStatement statement = con.prepareStatement("insert into xworkz_people values(?, ?, ?, ?)");
+            statement.setString(1, name);
+            statement.setInt(2, yopi);
+            statement.setString(3, education);
+            statement.setString(4, type);
+            statement.executeUpdate();
+            statement.close();
+            con.close();
+            PrintWriter out = response.getWriter();
+            out.println("<html><body><h1 style='color:red'><b>Successfully Inserted"
+                        + "</b></h1></body></html>");
+		}catch(Exception e) {
+			
+		}
 		
 		
 		
